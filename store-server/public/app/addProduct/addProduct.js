e774438fe751aa9891fab47cfbb46a2818531cd7
RubenCordeiro <ruben.cordeiro.eng@gmail.com>
2015-06-02 18:23:43 +0100

    Store-server: update add product view

--- store-server/public/app/addProduct/addProduct.js
+++ store-server/public/app/addProduct/addProduct.js
@@ -10,7 +10,8 @@ angular.module('BookshelfApp.addProduct', ['ui.router', 'BookshelfApp.root', 'ui
         });
     }])
 
-    .controller('AddProductCtrl', ['$scope', '$modal', '$q', 'BookModel', function ($scope, $modal, $q, BookModel) {
+    .controller('AddProductCtrl', ['$scope', '$modal', '$q', 'BookModel', 'SweetAlert', 'lodash',
+        function ($scope, $modal, $q, BookModel, SweetAlert, lodash) {
         /* sidebar hurdles */
         transparent = false;
         $('nav[role="navigation"]').removeClass('navbar-transparent').addClass('navbar-default'); // oh, no... Jquery!
@@ -38,10 +39,26 @@ angular.module('BookshelfApp.addProduct', ['ui.router', 'BookshelfApp.root', 'ui
             $scope.book.stock = 0;
             if (!$scope.book.thumbnail)
                 $scope.book.thumbnail = 'http://lorempixel.com/128/189/abstract/no%20thumbnail/';
+            $scope.book.isbn = item.isbn.identifier;
         };
 
         $scope.createBook = function() {
-
+            BookModel.create($scope.book)
+            .then(function(NewBook) {
+                SweetAlert.swal({
+                        title: "Success",
+                        text: "The book item was successfully created",
+                        type: "success",
+                        showCancelButton: false,
+                        confirmButtonText: "Ok",
+                        closeOnConfirm: true},
+                    function(){
+                        console.log("Entered:", NewBook);
+                    });
+            })
+            .catch(function(message) {
+                SweetAlert.swal("Error", message, "error");
+            })
         };
 
         $scope.selected = null;
