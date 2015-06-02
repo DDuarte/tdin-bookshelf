e4cdad5a036332bf41dfc3022ba3efb5a4ffdde6
DDuarte <dnpd.dd@gmail.com>
2015-06-03 00:36:27 +0100

    Warehouse: Define user model

--- warehouse/api/models/User.js
+++ warehouse/api/models/User.js
@@ -8,7 +8,47 @@
 module.exports = {
 
   attributes: {
+// The user's full name
+    // e.g. John Doe
+    name: {
+      type: 'string',
+      required: true
+    },
 
+    // The user's job description
+    // e.g. Clerk
+    description: {
+      type: 'string'
+    },
+
+    // The user's email address
+    // e.g. nikola@tesla.com
+    email: {
+      type: 'string',
+      email: true,
+      required: true,
+      unique: true
+    },
+
+    // The encrypted password for the user
+    // e.g. asdgh8a249321e9dhgaslcbqn2913051#T(@GHASDGA
+    encryptedPassword: {
+      type: 'string',
+      required: true
+    },
+
+    // The timestamp when the the user last logged in
+    // (i.e. sent a username and password to the server)
+    lastLoggedIn: {
+      type: 'date',
+      required: true,
+      defaultsTo: new Date(0)
+    },
+
+    // url for gravatar
+    gravatarUrl: {
+      type: 'string'
+    }
   }
 };
 
--- warehouse/assets/js/public/signup/SignupController.js
+++ warehouse/assets/js/public/signup/SignupController.js
@@ -24,11 +24,14 @@ angular.module('SignupModule').controller('SignupController', ['$scope', '$http'
 
         // Handle known error type(s).
         // If using sails-disk adapter -- Handle Duplicate Key
-        var emailAddressAlreadyInUse = sailsResponse.status == 409;
+        var emailAddressAlreadyInUse = sailsResponse.status === 409;
 
         if (emailAddressAlreadyInUse) {
           toastr.error('That email address has already been taken, please try again.', 'Error');
+          return;
         }
+
+        toastr.error('An unexpected error occurred, please try again.', 'Error');
       })
       .finally(function eitherWay() {
         $scope.signupForm.loading = false;
