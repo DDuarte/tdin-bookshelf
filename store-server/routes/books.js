52b3399a563285bcaac8bfd596a822eed4f4a080
RubenCordeiro <ruben.cordeiro.eng@gmail.com>
2015-06-02 18:22:33 +0100

    Store-server: add books routes

---
+++ store-server/routes/books.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var Joi = require('joi'),
+    Models = require('../models'),
+    Boom = require('boom');
+
+module.exports = function(server) {
+    server.route({
+        path: '/api/books',
+        method: 'POST',
+        config: {
+            tags: ['api'],
+            validate: {
+                payload: {
+                    title: Joi.string().required(),
+                    description: Joi.string().required(),
+                    isbn: Joi.string().required(),
+                    stock: Joi.number().required(),
+                    price: Joi.number().required(),
+                    thumbnail: Joi.string().required()
+                }
+            },
+            handler: function(request, reply) {
+                Models.Book.findOne({
+                    where: {
+                        ISBN: request.payload.isbn
+                    }
+                })
+                .then(function(Book) {
+                    if (Book)
+                        return reply(Boom.badRequest("A book with the given ISBN already exists"));
+
+                    Models.Book.create({
+                        title: request.payload.title,
+                        description: request.payload.description,
+                        ISBN: request.payload.isbn,
+                        stock: request.payload.stock,
+                        price: request.payload.price,
+                        thumbnail: request.payload.thumbnail
+                    })
+                    .then(function(NewBook) {
+                        return reply(NewBook.dataValues);
+                    })
+                    .catch(function(Error) {
+                        console.log("Error:", Error);
+                        return reply(Boom.badImplementation("Internal server error"));
+                    });
+                })
+                .catch(function(error) {
+                    console.log("Error:", error);
+                    return reply(Boom.badImplementation("Internal server error"));
+                });
+            }
+        }
+    });
+
+    server.route({
+        path: '/api/books/latest',
+        method: 'GET',
+        config: {
+            tags: ['api'],
+            handler: function(request, reply) {
+                Models.Book.findAll({
+
+                })
+            }
+        }
+    });
+};
--- store-server/routes/index.js
+++ store-server/routes/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = function(server) {
-
     require('./auth')(server);
     require('./users')(server);
     require('./orders')(server);
     require('./search')(server);
+    require('./books')(server);
 };
