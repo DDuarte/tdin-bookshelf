f10af8a2b68a976c07df992c43128bacafa32b17
RubenCordeiro <ruben.cordeiro.eng@gmail.com>
2015-06-03 20:28:08 +0100

    Store-client: add checkout operation to the checkout page

--- store-server/public/app/checkout/checkout.js
+++ store-server/public/app/checkout/checkout.js
@@ -10,7 +10,8 @@ angular.module('BookshelfApp.checkout', ['ui.router', 'BookshelfApp.root', 'ui.b
         });
     }])
 
-    .controller('CheckoutCtrl', ['$scope', 'ngCart', function ($scope, ngCart) {
+    .controller('CheckoutCtrl', ['$scope', 'sessionService', 'SweetAlert', 'ngCart', 'lodash', 'OrderModel',
+        function ($scope, sessionService, SweetAlert, ngCart, lodash, OrderModel) {
         /* sidebar hurdles */
         transparent = false;
         $('nav[role="navigation"]').removeClass('navbar-transparent').addClass('navbar-default'); // oh, no... Jquery!
@@ -22,9 +23,22 @@ angular.module('BookshelfApp.checkout', ['ui.router', 'BookshelfApp.root', 'ui.b
         $scope.removeCartItem = function(index) {
             ngCart.removeItem(index);
             $scope.cartItems = ngCart.getItems();
-        }
+        };
 
         $scope.checkout = function() {
+            var items = lodash.map($scope.cartItems, function(item) {
+                return {
+                    id: item.getId(),
+                    quantity: item.getQuantity()
+                }
+            });
 
+            OrderModel.create(sessionService.user.id, {items: items})
+            .then(function(Order) {
+                SweetAlert.swal("Order successfully created", "", "success");
+            })
+            .catch(function(error) {
+                SweetAlert.swal("Error creating order", error, "error");
+            });
         }
     }]);
