a3f30eb8d644500ca1e5f497a8dc18b815437914
RubenCordeiro <ruben.cordeiro.eng@gmail.com>
2015-06-03 20:27:05 +0100

    Store-server: add POST /api/users/{userId}/orders route

--- store-server/routes/orders.js
+++ store-server/routes/orders.js
@@ -1,6 +1,10 @@
 'use strict';
 
-var Joi = require('joi');
+var Joi = require('joi'),
+    Boom = require('boom'),
+    _ = require('lodash'),
+    Models = require('../models'),
+    Promise = require('bluebird');
 
 module.exports = function(server) {
 
@@ -25,20 +29,74 @@ module.exports = function(server) {
         method: 'POST',
         config: {
             tags: ['api'],
+            auth: 'token',
             validate: {
                 params: {
-                    userId: Joi.number()
+                    userId: Joi.number().required()
                 },
                 payload: {
                     items: Joi.array().items(Joi.object().keys({
                             id: Joi.number(),
-                            quantity: Joi.number().integer()
+                            quantity: Joi.number()
                         }
-                    ))
+                    )).required()
                 }
             },
             handler: function(request, reply) {
-                reply("Not yet implemented");
+
+                if (request.auth.credentials.id != request.params.userId)
+                    return reply(Boom.unauthorized("No permission"));
+
+                Models.sequelize.transaction().then(function (t) {
+                    Models.Customer.findOne({
+                        where: {
+                            id: request.auth.credentials.id
+                        }
+                    }, {transaction: t})
+                    .then(function(Customer) {
+                        Models.Order.create({
+                            state: 'waiting',
+                            CustomerId: request.auth.credentials.id
+                        }, {transaction: t})
+                        .then(function(NewOrder) {
+                            Promise.map(request.payload.items, function(item) {
+                                console.log("Mapping items");
+                                return Models.Book.findOne({
+                                    where: {
+                                        id: item.id
+                                    }
+                                });
+                            }).then(function(Books) {
+                                for (var i = 0; i < Books.length; ++i) {
+                                    Books[i].OrderBook = {
+                                        quantity: request.payload.items[i].quantity
+                                    }
+                                }
+
+                                NewOrder.setBooks(Books, {transaction: t})
+                                    .then(function() {
+                                        console.log("After set books");
+                                        t.commit();
+                                        return reply(NewOrder.dataValues);
+                                    })
+                                    .catch(function(error) {
+                                        console.log("Error:", error);
+                                        t.rollback();
+                                        return reply(Boom.badImplementation("Internal server error"));
+                                    });
+                            }).catch(function(error) {
+                                console.log("Error:", error);
+                                t.rollback();
+                                return reply(Boom.badImplementation("Internal server error"));
+                            });
+                        })
+                    })
+                    .catch(function(error) {
+                        console.log("Error:", error);
+                        t.rollback();
+                        return reply(Boom.badImplementation("Internal server error"));
+                    });
+                });
             }
         }
     });
