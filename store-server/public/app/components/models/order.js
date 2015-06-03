7499d48df785e0ce281ad3f3c20764ec9503fb87
RubenCordeiro <ruben.cordeiro.eng@gmail.com>
2015-06-03 23:07:44 +0100

    Store-client: implement myOrders view and add myOrders_details view

--- store-server/public/app/components/models/order.js
+++ store-server/public/app/components/models/order.js
@@ -18,4 +18,32 @@ angular.module('BookshelfApp.models.order', [])
             return deferred.promise;
         };
 
+        this.getAllFromUser = function(userId) {
+            var deferred = $q.defer();
+
+            $http.get(ServerConfig.baseUrl + '/users/' + userId + '/orders')
+            .then(function(result) {
+                return deferred.resolve(result.data);
+            })
+            .catch(function(error) {
+                return deferred.reject(error.data.message);
+            });
+
+            return deferred.promise;
+        };
+
+        this.getFromUser = function(userId, orderId) {
+            var deferred = $q.defer();
+
+            $http.get(ServerConfig.baseUrl + '/users/' + userId + '/orders/' + orderId)
+            .then(function(result) {
+                return deferred.resolve(result.data);
+            })
+            .catch(function(error) {
+                return deferred.reject(error.data.message);
+            });
+
+            return deferred.promise;
+        };
+
     }]);
--- store-server/public/app/myOrders/myOrders.js
+++ store-server/public/app/myOrders/myOrders.js
@@ -8,13 +8,44 @@ angular.module('BookshelfApp.myOrders', ['ui.router', 'BookshelfApp.root', 'ui.b
             templateUrl: 'myOrders/myOrders.html',
             controller: 'MyOrdersCtrl'
         });
+
+        $stateProvider.state('root.myOrdersDetails', {
+            url: 'myOrders/:id',
+            templateUrl: 'myOrders/myOrders_details.html',
+            controller: 'MyOrdersDetailsCtrl'
+        });
     }])
 
-    .controller('MyOrdersCtrl', ['$scope', function ($scope) {
+    .controller('MyOrdersCtrl', ['$scope', '$state', 'SweetAlert', 'sessionService', 'OrderModel', function ($scope, $state, SweetAlert, sessionService, OrderModel) {
         /* sidebar hurdles */
         transparent = false;
         $('nav[role="navigation"]').removeClass('navbar-transparent').addClass('navbar-default'); // oh, no... Jquery!
         $(window).off('scroll');
 
+        OrderModel.getAllFromUser(sessionService.user.id)
+        .then(function(Orders) {
+            $scope.orderItems = Orders;
+            console.log("Orders:", $scope.orderItems);
+        })
+        .catch(function(error) {
+            SweetAlert.swal("Error", error, "error");
+        });
 
+        $scope.seeDetails = function(orderItem) {
+            $state.go('root.myOrdersDetails', {id: orderItem.order.id});
+        };
+    }])
+    .controller('MyOrdersDetailsCtrl', ['$scope', '$state', '$stateParams', 'sessionService', 'SweetAlert', 'OrderModel', 'lodash',
+        function($scope, $state, $stateParams, sessionService, SweetAlert, OrderModel, lodash) {
+        OrderModel.getFromUser(sessionService.user.id, $stateParams.id)
+        .then(function(Order) {
+            $scope.order = Order;
+            $scope.order.total = lodash.reduce($scope.order.items, function(total, item) {
+                return total + (item.price * item.OrderBook.quantity);
+            }, 0);
+            console.log("Order:", $scope.order);
+        })
+        .catch(function(error) {
+            SweetAlert.swal("Error", error, "error");
+        });
     }]);
